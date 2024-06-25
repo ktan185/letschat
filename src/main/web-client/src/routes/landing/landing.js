71005bbb96b434b3f878c202e0acb33e777e42bd
Kay Tang <126426690+ktan185@users.noreply.github.com>
2024-06-26 11:52:37 +1200

    feat: updated landing page background image

--- src/main/web-client/src/routes/landing/landing.js
+++ src/main/web-client/src/routes/landing/landing.js
@@ -1,7 +1,13 @@
 import styles from './landing.module.css'
 
 function Landing() {
-  return <div styles={styles.body}></div>
+  return (
+    <div className={styles.body}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Lets Chat!</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
