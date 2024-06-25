710ddcbc6f2b9a48420a372d2a1fea7ed70daf41
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2024-06-25 22:28:20 +1200

    made it so u can find all chats and did error handling in the controller class

--- src/main/java/com/nz/letschat/controller/ChatController.java
+++ src/main/java/com/nz/letschat/controller/ChatController.java
@@ -1,10 +1,12 @@
 package com.nz.letschat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import com.nz.letschat.model.Chat;
 import com.nz.letschat.repository.ChatRepository;
 
@@ -14,7 +16,26 @@ public class ChatController {
     ChatRepository chatRepository;
 
     @PostMapping("/addChat")
-    public void addChat(@RequestBody Chat chat){
-        chatRepository.save(chat);
+    public ResponseEntity<Chat> addChat(@RequestBody Chat chat){
+        if(chatRepository.findChatByChatIDExists(chat.getChatID().getOwnerEmailAddress(), chat.getChatID().getUniqueChatID())){
+            return ResponseEntity.badRequest().build();
+        }
+        try{
+            chatRepository.save(chat);
+            return ResponseEntity.ok(chat);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/chats")
+    public ResponseEntity<List<Chat>> getAllChats(){
+        try{
+            List<Chat> allChats=chatRepository.findAll();
+            return ResponseEntity.ok(allChats);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 }
--- src/main/java/com/nz/letschat/repository/ChatRepository.java
+++ src/main/java/com/nz/letschat/repository/ChatRepository.java
@@ -1,9 +1,12 @@
 package com.nz.letschat.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.nz.letschat.model.Chat;
 
 public interface ChatRepository extends MongoRepository<Chat,String>{
-    
+    public List<Chat> findAll();
+    public boolean findChatByChatIDExists(String ownerEmailAddress,String uniqueChatID);
 }
