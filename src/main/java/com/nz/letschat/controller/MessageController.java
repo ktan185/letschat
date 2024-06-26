1dd019a827d6b98ef23619e1b53b9225a5a52e6b
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2024-06-26 12:51:30 +1200

    added backend for websockets for communication

---
+++ src/main/java/com/nz/letschat/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.nz.letschat.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.nz.letschat.model.Message;
+
+@Controller
+
+public class MessageController {
+    @Autowired 
+    private SimpMessagingTemplate simpMessagingTemplate;
+    
+    @MessageMapping("/chat")
+    public void send(Message message) throws Exception {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        message.setTime(time);
+        //temporary handler to test
+        // message.setChatID(message.getFrom());
+        simpMessagingTemplate.convertAndSend("/topic/messages/"+message.getChatID(),message);
+    }
+    
+}
