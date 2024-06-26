2c2ae815a0e3d77e15fe72008bb51fa74c54b18f
Hosea Tong-Ho <hton892@aucklanduni.ac.nz>
2024-06-27 00:27:54 +1200

    lists all chats on home page

--- src/main/web-client/src/routes/home/home.js
+++ src/main/web-client/src/routes/home/home.js
@@ -1,11 +1,35 @@
-import styles from './home.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './home.module.css';
+import { getChats } from '../../services/chatService';
 
 function Home() {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      const chatsData = await getChats();
+      setChats(chatsData);
+    };
+
+    fetchChats();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1>Welcome back!</h1>
+      <div className={styles.chats}>
+        {chats.length > 0 ? (
+          chats.map(chat => (
+            <div key={chat.chatID} className={styles.chat}>
+              <h2>{chat.chatName}</h2>
+            </div>
+          ))
+        ) : (
+          <h2>No Chats Available</h2>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
