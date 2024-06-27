ac1532519867c18d817edba9661528bde59147bf
Kay Tang <tangkay10k@gmail.com>
2024-06-27 14:43:27 +1200

    feat: created sign up card
    - Can now toggle between signing in and up.

--- src/main/web-client/src/components/login/loginComponents.js
+++ src/main/web-client/src/components/login/loginComponents.js
@@ -7,10 +7,10 @@ import { signIn } from '../../services/authService'
 import { useSession } from '../../contexts/SessionContext'
 import { useNavigate } from 'react-router-dom'
 
-function LoginForm() {
+export function LoginForm() {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
-  const { session, updateSession } = useSession()
+  const { updateSession } = useSession()
   const navigate = useNavigate()
 
   const handleSignIn = async (e) => {
@@ -69,4 +69,92 @@ function LoginForm() {
   )
 }
 
-export default LoginForm
+export function SignUpForm() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value)
+  }
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value)
+  }
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+    console.log('signing up!')
+  }
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Sign Up</Card.Title>
+        <Form>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your Email Address"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="userName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Select a username"
+              value={userName}
+              onChange={handleUserNameChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="firstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="lastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </Form.Group>
+          <br />
+          <Button variant="primary" onClick={handleSignUp}>
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  )
+}
--- src/main/web-client/src/routes/landing/landing.js
+++ src/main/web-client/src/routes/landing/landing.js
@@ -1,13 +1,34 @@
-import LoginForm from '../../components/login/loginComponents'
+import Button from 'react-bootstrap/esm/Button'
+import { SignUpForm, LoginForm } from '../../components/login/loginComponents'
 import styles from './landing.module.css'
+import { useState } from 'react'
 
 function Landing() {
+  const [isSignUpCard, setSignUp] = useState(false)
+
+  const toggleSignUp = (e) => {
+    setSignUp(!isSignUpCard)
+  }
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
         <h1 className={styles.title}>Lets Chat!</h1>
         <div className={styles.signInContainer}>
-          <LoginForm />
+          {isSignUpCard ? (
+            <>
+              <SignUpForm />
+              <a>Have an account? </a>
+            </>
+          ) : (
+            <>
+              <LoginForm />
+              <a>Don't have an account? </a>
+            </>
+          )}
+          <Button onClick={toggleSignUp}>
+            {isSignUpCard ? 'Sign In' : 'Sign Up now!'}
+          </Button>
         </div>
       </div>
     </div>
