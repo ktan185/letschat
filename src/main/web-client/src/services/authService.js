f163af8bb77f4b19a233f1a1e815bc2be9027fdf
Kay Tang <tangkay10k@gmail.com>
2024-06-28 12:46:19 +1200

    feat: Updated chat creation API
    - Chat ID is now based on user token

--- src/main/web-client/src/services/authService.js
+++ src/main/web-client/src/services/authService.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 const SERVER_PORT = 'http://localhost:8080'
 
 export const signIn = async (userDetails) => {
-  return await axios.post(`${SERVER_PORT}/signIn`, userDetails)
+  return await axios.post(`${SERVER_PORT}/api/signIn`, userDetails)
 }
 
 export const signUp = async (newUserDetails) => {
-  return await axios.post(`${SERVER_PORT}/signUp`, newUserDetails)
+  return await axios.post(`${SERVER_PORT}/api/signUp`, newUserDetails)
 }
--- src/main/web-client/src/services/chatService.js
+++ src/main/web-client/src/services/chatService.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const SERVER_PORT = 'http://localhost:8080'
 
 export const getChats = async () => {
-    try {
-      const response = await axios.get(`${SERVER_PORT}/chats`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching chats:", error);
-      return [];
-    }
+  try {
+    const response = await axios.get(`${SERVER_PORT}/chats`)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching chats:', error)
+    return []
   }
+}
