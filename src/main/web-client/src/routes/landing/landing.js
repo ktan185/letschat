20dea5b209705f11a07686bdcac14c3f81d47ea3
Kay Tang <tangkay10k@gmail.com>
2024-06-28 17:43:03 +1200

    feat:  updated landing page UI

--- src/main/web-client/src/routes/landing/landing.js
+++ src/main/web-client/src/routes/landing/landing.js
@@ -1,4 +1,3 @@
-import Button from 'react-bootstrap/esm/Button'
 import { SignInSignUpCard } from '../../components/login/loginComponents'
 import styles from './landing.module.css'
 import { useState } from 'react'
@@ -11,18 +10,13 @@ function Landing() {
   }
 
   return (
-    <div className={styles.body}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Lets Chat!</h1>
-        <div className={styles.signInContainer}>
-          <SignInSignUpCard
-            isSignUpCard={isSignUpCard}
-            toggleSignUp={toggleSignUp}
-          />
-          <Button onClick={toggleSignUp}>
-            {isSignUpCard ? 'Sign In' : 'Sign Up now!'}
-          </Button>
-        </div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lets Chat!</h1>
+      <div className={styles.signInContainer}>
+        <SignInSignUpCard
+          isSignUpCard={isSignUpCard}
+          toggleSignUp={toggleSignUp}
+        />
       </div>
     </div>
   )
