eea9d8be14dba7f1e3226bcf6b234a9320424510
Kay Tang <tangkay10k@gmail.com>
2024-06-30 16:57:10 +1200

    feat: updated sign up form UX

--- src/main/web-client/src/components/login/utils/login.js
+++ src/main/web-client/src/components/login/utils/login.js
@@ -1,4 +1,33 @@
 export function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/
-  return re.test(email)
+  const emailRegex =
+    /^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  return emailRegex.test(email)
+}
+
+export function detailsValidation(userDetails) {
+  if (!validateEmail(userDetails.email)) {
+    alert('Please insert a valid email address.')
+    return false
+  }
+
+  if (userDetails.firstName === '') {
+    alert('Please include your first name')
+    return false
+  }
+
+  if (userDetails.lastName === '') {
+    alert('Please include your last name')
+    return false
+  }
+
+  if (userDetails.userName === '') {
+    alert('Please include your a username')
+    return false
+  }
+
+  if (userDetails.password === '') {
+    alert('Your password cannot be empty')
+    return false
+  }
+  return true
 }
